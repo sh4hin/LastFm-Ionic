4271f6a4ee9bf349bd6aef4081a9a3c3d4e04601
Amir.H Shahin <amir.shaahin@gmail.com>
2014-10-31 17:43:25 +0800

    V1.0

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
 angular.module('services', ['ngResource'])
     .factory('LastFm',function ($resource) {
         return $resource('http://ws.audioscrobbler.com/2.0/?method=:op&artist=:artist&country=:country&api_key=:key&format=json', {},
-            {get: {method: 'GET', params: {key: 'fe77a75f00e2a33dab9cf97b10bb482a'}}});
+            {get: {method: 'GET', params: {key: 'Put Your LastFm API'}}});
     });
